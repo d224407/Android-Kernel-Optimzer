1993ba7855f3da5e2259625ccf427fffdda326ac
d224407 <nguynlanh63@gmail.com>
2026-07-04 10:28:52 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -2,13 +2,7 @@
 
 MODDIR=${0%/*}
 
-# Chờ hệ thống boot hoàn tất
-while [ "$(getprop sys.boot_completed)" != "1" ]; do
-    sleep 5
-done
-sleep 10
-
-# Chạy script tối ưu kernel (chạy ngầm, không chặn tiến trình)
+# Chạy script tối ưu (không cần chờ boot vì đã có trong script)
 nohup sh $MODDIR/kernelenhance.sh > /dev/null 2>&1 &
 
 echo "KernelEnhancer started at $(date)" > /data/local/tmp/status.log
